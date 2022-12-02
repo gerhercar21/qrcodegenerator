aa51acaee9efeccc1ba9a97f32196124bfa61b47
Gerardo <gerardohdezcarrera@gmail.com>
2022-12-02 13:08:00 -0600

    changes label

--- validation.js
+++ validation.js
@@ -3,8 +3,8 @@ const button = document.querySelector('#btngenera');
 
 button.addEventListener('click', ()=>{
     if(input.value.length == 0){
-        document.getElementById('mensaje').innerHTML='<p style="font-family: univia-pro; font-size:1rem; margin-top:-1%; color:red; ">Debes agregar algo...</p>';
+        document.getElementById('mensaje').innerHTML='<label style="font-family: univia-pro; font-size:1rem; margin-top:-1%; color:red; ">Debes agregar algo...</label>';
     }else{
-        document.getElementById('mensaje').innerHTML='<p style="font-family: univia-pro; font-size:1rem; margin-top:-1%; color:green;">Descarga tu QR</p>';
+        document.getElementById('mensaje').innerHTML='<label style="font-family: univia-pro; font-size:1rem; margin-top:-1%; color:green;">Descarga tu QR</label>';
     }
 });
